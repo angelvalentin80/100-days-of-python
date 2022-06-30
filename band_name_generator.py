e2a9f43e296e57953df0028f3801c75b1b520e87
Angel Valentin <ajrocketmail@gmail.com>
2022-06-29 21:33:49 -0400

    day1 - Simple string concatenation, variables basics, and swapping two variables

---
+++ band_name_generator.py
@@ -0,0 +1,19 @@
+#my solution
+print("Welcome to the Band Name Generator")
+
+city = input("What city did you grow up in?\n")
+pet = input("What is the name of a pet?\n")
+
+print("Your band name could be " + city.title() + " " + pet.title())
+
+#swapping two variables
+
+a = 1
+b = 20
+
+c = a
+a = b
+b = c
+
+print("a is now: " + str(a))
+print("b is now: " + str(b))
