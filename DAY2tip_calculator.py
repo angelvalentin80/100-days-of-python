c444a959c7d3624177a5d0781710c3ff6f500e8c
Angel Valentin <ajrocketmail@gmail.com>
2022-06-30 16:39:38 -0400

    edited so that the strings change to their respective data types after the input is typed so we aren't constantly changing the type

--- DAY2tip_calculator.py
+++ DAY2tip_calculator.py
@@ -1,13 +1,13 @@
 
 # my solution
 print("Welcome to the tip calculator.")
-total_bill = input("What was the total bill?")
-percentage_tip = input("What percentage tip would you like to give? 10, 12, or 15?")
-number_of_people = input("How many people to split the bill?")
+total_bill = float(input("What was the total bill?"))
+percentage_tip = float(input("What percentage tip would you like to give? 10, 12, or 15?"))
+number_of_people = int(input("How many people to split the bill?"))
 
-tip = float(total_bill) * (float(percentage_tip) / 100)
-total = float(total_bill) + tip
-split = total / float(number_of_people)
+tip = total_bill * (percentage_tip / 100)
+total = total_bill + tip
+split = total / number_of_people
 print(f"Each person should pay: ${round(split, 2)}")
 
 
