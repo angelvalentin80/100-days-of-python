b1c0f8ace6be4e0c3f1c4d0dc88793b002807a1c
Angel Valentin <ajrocketmail@gmail.com>
2022-07-09 15:45:16 -0400

    Fixed a bug where we were automatically returning the last name instead of actually finding the right name that had the highest price. Also reformmated the code so that it works with a dictionary and not a list of dictionaries

--- DAY9secret_auction.py
+++ DAY9secret_auction.py
@@ -1,25 +1,30 @@
-# my solution
+# Instructors Way. She wants to make the key as the name and the value as the bid
 import os
 
-people_in_auction = []  # empty dictionary that we will have to add into once we retrieve information
+people_in_auction = {}  # empty dictionary that we will have to add into once we retrieve information
 
 def save_auction_details(name = str, bid = int):
-    people_in_auction.append({"name": name, "bid": bid}) 
+    people_in_auction[name] = bid
 
-def check_highest_bid():
+def check_highest_bid() -> int:
     highest_bid = 0
-    for i in people_in_auction:
-        bid = i["bid"]
+
+    for key in people_in_auction:
+        bid = people_in_auction[key]
         if bid > highest_bid:
             highest_bid = bid
     return highest_bid
 
 
+def get_highest_bid_name(highest_bid) -> str:
+    for key in people_in_auction:
+        if people_in_auction[key] == highest_bid:
+            return key
 
-auction_ongoing = True
+
+auction_ongoing = True # temp
 
 while auction_ongoing:
-    
     print("Welcome to the secret auction program.\n")
 
     try:
@@ -38,6 +43,7 @@ while auction_ongoing:
         continue
     elif question.lower() == "no":
         highest_bid = check_highest_bid()
+        name = get_highest_bid_name(highest_bid)
         os.system('clear') # clearing the console . Forgot this existed.
         print(f"The winner is {name.capitalize()} with a bid of {highest_bid}")  
         auction_ongoing = False
